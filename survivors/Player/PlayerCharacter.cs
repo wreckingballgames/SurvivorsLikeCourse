dc250af7dcdf83eb0d8c8082ad06fd2e8cb5088a
Raymond McDonald <raymond@raymondmcdonald.net>
2024-11-11 12:31:13 -0600

    Create `Main` scene and implement basic movement

--- survivors/Player/PlayerCharacter.cs
+++ survivors/Player/PlayerCharacter.cs
@@ -4,43 +4,23 @@ namespace Survivors.Player;
 
 public partial class PlayerCharacter : CharacterBody3D
 {
-	[Export]
-	public float Speed = 5.0f;
-	[Export]
-	public float JumpVelocity = 4.5f;
+    [Export]
+    public float MaxMoveSpeed { get; private set; }
 
-	public override void _PhysicsProcess(double delta)
-	{
-		Vector3 velocity = Velocity;
+    public override void _PhysicsProcess(double delta)
+    {
+        base._PhysicsProcess(delta);
 
-		// Add the gravity.
-		if (!IsOnFloor())
-		{
-			velocity += GetGravity() * (float)delta;
-		}
+        Velocity = GetMovementVector() * MaxMoveSpeed;
+        MoveAndSlide();
+        GD.Print(Velocity);
+    }
 
-		// Handle Jump.
-		if (Input.IsActionJustPressed("ui_accept") && IsOnFloor())
-		{
-			velocity.Y = JumpVelocity;
-		}
+    private Vector3 GetMovementVector()
+    {
+        float forwardMovement = Input.GetAxis("move_backward", "move_forward");
+        float rightMovement = Input.GetAxis("strafe_left", "strafe_right");
 
-		// Get the input direction and handle the movement/deceleration.
-		// As good practice, you should replace UI actions with custom gameplay actions.
-		Vector2 inputDir = Input.GetVector("ui_left", "ui_right", "ui_up", "ui_down");
-		Vector3 direction = (Transform.Basis * new Vector3(inputDir.X, 0, inputDir.Y)).Normalized();
-		if (direction != Vector3.Zero)
-		{
-			velocity.X = direction.X * Speed;
-			velocity.Z = direction.Z * Speed;
-		}
-		else
-		{
-			velocity.X = Mathf.MoveToward(Velocity.X, 0, Speed);
-			velocity.Z = Mathf.MoveToward(Velocity.Z, 0, Speed);
-		}
-
-		Velocity = velocity;
-		MoveAndSlide();
-	}
+        return new Vector3(rightMovement, 0.0f, forwardMovement).Normalized();
+    }
 }
